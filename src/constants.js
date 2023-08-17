d3062bafb4b934eb1c102fee611e7897c2263d41
Shrayansh Jain <jainshrayansh1998@gmail.com>
2023-08-17 19:21:44 +0530

    api fetching

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "4e0dc683cff980b1475d211ca5ddf7d5";
+export const BASE_URL = "https://api.themoviedb.org/3";
---
+++ src/utility/api.js
@@ -0,0 +1,40 @@
+import { BASE_URL, API_KEY } from "../constants";
+
+export const fetchUpcomingMovies = async (page) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=${page}`
+    );
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching upcoming movies:", error);
+    return [];
+  }
+};
+
+export const fetchMovieDetails = async (movieId) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    return null;
+  }
+};
+
+export const searchMovies = async (query, page) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=${page}`
+    );
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error searching movies:", error);
+    return [];
+  }
+};
